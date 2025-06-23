f75da0f7e94dba8d7b88fbd88d46a41536e99cf9
George Paun <georgemg1187@gmail.com>
2025-06-23 11:23:01 +0300

    removed tesintg folder

--- snippets/test.js
+++
@@ -1,7 +0,0 @@
-function runTest() {
-    console.log('asa nu');
-    console.log('asa')
-    console.table({name: 'Gege'})
-    console.log(1)
-    //testing dismiss after new commit on approve PR
-}
