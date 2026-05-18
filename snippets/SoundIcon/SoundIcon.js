10123157da97172dfcee134abea4e93765abd1c5
George Paun <georgemg1187@gmail.com>
2026-05-18 10:31:24 +0300

    sound icon fix with new method

--- snippets/SoundIcon/SoundIcon.js
+++ snippets/SoundIcon/SoundIcon.js
@@ -5,9 +5,9 @@
  *****************************************/
 var soundConfig = {
     css: {
-        'left': '60', //Position on X axis. Leave just the quotes for default
-        'top': '60', //Position on Y axis. Leave just the quotes for default
-        'background-image': 'blackSoundIcon_svg', //Name of the svg file goes between the quotes. Leave just the quotes if you don't wish to change sound color
+        'left': '20', //Position on X axis.
+        'top': '20', //Position on Y axis.
+        'background-image': '', //Name of the svg file goes between the quotes. Leave just the quotes if you don't wish to change sound color
     },
     'screensToHideSoundIcon': [Screen1, Screen2], //Name of the screens the sound icon should not be displayed; Separate them with comma. Leave it empty for default
     'videosToShowSoundIconOn': [MainVideo] //Name of the Video the sound icon should be displayed; Separate them with comma. Leave it empty for default
@@ -28,7 +28,7 @@ function getCss(config) {
 
     function addPx(prop) {
         if (isNaN(prop)) return prop;
-        
+
         return prop + 'px';
     }
 
@@ -58,7 +58,7 @@ const getTeadsApi = () => {
 function addSoundIcon(config) {
     const { css, screensToHideSoundIcon, videosToShowSoundIconOn } = config;
 
-    const soundBtnHiddenCss = {display: 'none', width: 10 }
+    const soundBtnHiddenCss = { display: 'none', width: 10 }
     const additionalCss = { right: 'auto', bottom: 'auto', transform: 'translateX(-50%) translateY(-50%)', padding: '10px 8px' }
     const soundBntVisibleCss = { ...css, ...additionalCss }
 
@@ -66,11 +66,23 @@ function addSoundIcon(config) {
 
     const { left, top, ...soundIconVisbleCss } = css;
 
-    soundIconVisbleCss['background-image'] = `url( ${bnt.LIBRARY[soundIconVisbleCss['background-image']].link}) !important`;
+    const url = `url( ${bnt.LIBRARY[soundIconVisbleCss['background-image']]?.link})`
+    if (soundIconVisbleCss['background-image'].trim().length) soundIconVisbleCss['background-image'] = url;
 
     const teadsApi = getTeadsApi();
 
     if (!teadsApi) return;
+
+    const defaultCallPlayerApiMethod = teadsApi.callPlayerApiMethod;
+
+    teadsApi.callPlayerApiMethod = function (...args) {
+        if (args[0] === 'customizeUI') return;
+
+        const finalArgs = [...args];
+        if (finalArgs[0] === 'customizeUI2') finalArgs[0] = 'customizeUI';
+
+        return defaultCallPlayerApiMethod.apply(this, finalArgs);
+    };
 
     //CN = className
     const soundBtnVissibleCN = `div.teads-ui-component-soundbutton ${getCss(soundBntVisibleCss)}`;
@@ -79,10 +91,10 @@ function addSoundIcon(config) {
     const soundIconVisibleCN = `.icon-muteoff, .icon-muteon ${getCss(soundIconVisbleCss)}`;
 
     // Apply custom sound icon if screen1 does not have other rules
-    teadsApi.callPlayerApiMethod('customizeUI', [soundBtnVissibleCN + soundIconVisibleCN]);
+    teadsApi.callPlayerApiMethod('customizeUI2', [soundBtnVissibleCN + soundIconVisibleCN]);
 
-    hideSound = () => { teadsApi.callPlayerApiMethod('customizeUI', [soundBtnHiddenCN]); console.log('asas') }
-    showSound = () => teadsApi.callPlayerApiMethod('customizeUI', [soundBtnVissibleCN + soundIconVisibleCN])
+    hideSound = () => { teadsApi.callPlayerApiMethod('customizeUI2', [soundBtnHiddenCN]); }
+    showSound = () => teadsApi.callPlayerApiMethod('customizeUI2', [soundBtnVissibleCN + soundIconVisibleCN])
 
     screensToHideSoundIcon.forEach(screen => {
         screen.onshow.addObserver(hideSound);
