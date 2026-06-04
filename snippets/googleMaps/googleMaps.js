7e1f86092733b20190aa6b23da294b974b6f7c92
Sharaf-Abusereya <sharaf.abusereya@gmail.com>
2026-06-04 11:05:12 +0300

    Remove locations from map configuration

--- snippets/googleMaps/googleMaps.js
+++ snippets/googleMaps/googleMaps.js
@@ -7,7 +7,6 @@ const mapConfig = {
         size: 40,
     },
     zoom: 14,
-    locations: getLocations(),
     mapStyle: getMapStyle(),
     google_map_id: "AIzaSyDe4AsA7y_plNGFuCddW_zPyrl7CEjtnzg",
 }
@@ -15,7 +14,7 @@ const mapConfig = {
 mapConfig.mapElement.onshowAnimationStart.addObserver(async function () {
     mapConfig.mapElement.onshowAnimationStart.removeObserver(arguments.callee);
 
-    const { mapElement, mapAssetName, customIcon, zoom, locations, mapStyle, google_map_id } = mapConfig;
+    const { mapElement, mapAssetName, customIcon, zoom, mapStyle, google_map_id } = mapConfig;
     let userLocation = await getUserLocation();
     
     if(customIcon)
@@ -24,7 +23,7 @@ mapConfig.mapElement.onshowAnimationStart.addObserver(async function () {
     const map = await addMap(mapElement, mapAssetName);
     listenMapEvents(map);
 
-    initMap({ map, userLocation, customIcon, zoom, locations, mapStyle, google_map_id });
+    initMap({ map, userLocation, customIcon, zoom, mapStyle, google_map_id });
 });
 
 function addMap(parent, mapAssetName) {
@@ -53,8 +52,8 @@ function addMap(parent, mapAssetName) {
     });
 }
 
-function initMap({ map, userLocation, customIcon, zoom, locations, mapStyle, google_map_id }){
-    map.contentWindow.postMessage(JSON.stringify({ type:"initMap", data: {userLocation, customIcon, zoom, locations, mapStyle, google_map_id } }), "*");
+function initMap({ map, userLocation, customIcon, zoom, mapStyle, google_map_id }){
+    map.contentWindow.postMessage(JSON.stringify({ type:"initMap", data: { userLocation, customIcon, zoom, mapStyle, google_map_id } }), "*");
 }
 
 function createIframe(){
@@ -67,11 +66,10 @@ function createIframe(){
 
 function listenMapEvents(map) {
     window.addEventListener("message", function (msg) {
-        console.log(msg);
         var data = JSON.parse(msg.data);
 
         if (data.type == "gmap" && data.data == "ready")
-            map.contentWindow.postMessage(JSON.stringify({ type: "initMap", data: { userLocation, zoom, google_map_id, locations, mapStyle, customIcon } }));
+            initMap({ map, userLocation, customIcon, zoom, mapStyle, google_map_id })
 
         switch (data.data) {
             case "track":
@@ -111,19 +109,6 @@ function getMapUrl(src, options) {
     const keys = Object.keys(options);
     let initialURL = bnt.LIBRARY[src].link + "?";
     return keys.reduce((acc, cur) => `${acc}${cur}=${options[cur]}&`, initialURL);
-}
-
-function getLocations() {
-    return [
-        { "name": "Louis Vuitton Hong Kong Landmark", "address": "Shops B6-B10, B23-B24 & B27-B30, B1/F, Landmark Atrium, Central <br>HK 00852, Hong Kong Island, Hong Kong SAR", "lat": 22.282120014978, "lon": 114.158312429005 },
-        { "name": "Louis Vuitton Hong Kong 5 Canton Road", "address": "Shop G005-006, Harbour City, TST <br>HK 00852, Kowloon, Hong Kong SAR", "lat": 22.2964767133108, "lon": 114.169076823713 },
-        { "name": "Louis Vuitton Hong Kong Airport", "address": "Unit 6E196 & 7E153, Terminal 1, Hong Kong International Airport <br>HK 00852, New Territories, Hong Kong SAR", "lat": 22.3149040617267, "lon": 113.933720981385 },
-        { "name": "Louis Vuitton Hong Kong Times Square", "address": "Shop 211-212, Level 2, Times Square,1 Matheson Street, Causeway Bay <br>HK 00852, Hong Kong Island, Hong Kong SAR", "lat": 22.2786185476679, "lon": 114.181830952784 },
-        { "name": "Louis Vuitton Hong Kong Elements", "address": "Shop 1020-1022, Elements, TST <br>HK 00852, Kowloon, Hong Kong SAR", "lat": 22.3043550192316, "lon": 114.161001852549 },
-        { "name": "Louis Vuitton Hong Kong Pacific Place", "address": "Shop 236 & Shop 351A, Pacific Place <br>HK 00852, Hong Kong Island, Hong Kong SAR", "lat": 22.2778262367903, "lon": 114.164935683468 },
-        { "name": "Louis Vuitton Hong Kong Lee Gardens", "address": "Shop G01, Lee Garden One, CWB <br>HK 00852, Hong Kong Island, Hong Kong SAR", "lat": 22.278597366676, "lon": 114.184890379533 },
-        { "name": "Louis Vuitton Hong Kong Peninsula", "address": "Shop E2-6, The Peninsula Hong Kong <br>HK 00852, Kowloon, Hong Kong SAR", "lat": 22.2953009149822, "lon": 114.171853694877 },
-    ];
 }
 
 function getMapStyle() {
