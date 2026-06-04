301bd42e13d26ec70630a76adbd31399e5061d25
Sharaf-Abusereya <sharaf.abusereya@gmail.com>
2026-06-04 10:59:54 +0300

    Update preview settings in Hover.js

--- templates/Hover/Hover.js
+++ templates/Hover/Hover.js
@@ -20,9 +20,8 @@ const hoverConfig = {
   },
 
   preview: {
-    enabled: true,              // true, to Reveal parts of backImage if user doesn't interact
+    delay: 2000,                // Milliseconds of inactivity before preview starts, false to deactivate preview
     duration: 1800,             // Animation duration in milliseconds
-    delay: 3000,                // Milliseconds of inactivity before preview starts
   },
 
 };
@@ -59,7 +58,7 @@ async function initHover(config) {
 
   frontElement.hide();
 
-  if (preview.enabled)
+  if (preview.delay !== false)
     getCoords = new PathGenerator({ duration: preview.duration, delay: preview.delay, points: getPoints(canvasSize) }).getPathCoords;
   else
     getCoords = getUserCoords;
